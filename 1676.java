435c62dd1474eab5d16b5272e07c76183a933c21
MrainW <wangrainm@gmail.com>
2021-10-07 09:11:04 -0400

    1676

---
+++ 1676.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode[] nodes) {
+        Set<Integer> set = new HashSet<>();
+        for (TreeNode t : nodes) set.add(t.val);
+        return dfs(root, set);
+    }
+    private TreeNode dfs(TreeNode root, Set<Integer> set){
+        if (root == null) return null;
+        if (set.contains(root.val)) return root;
+        TreeNode left = dfs(root.left, set);
+        TreeNode right = dfs(root. right, set);
+        if (left != null && right != null) return root;
+        if (left != null) return left;
+        if (right != null) return right;
+        return null;
+    }
+}
